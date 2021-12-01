465aebe02a98d6e084995dae89440c9dc046ea50
Ryan Block <rblock@gmail.com>
2021-11-30 16:00:38 -0800

    Clean up printer a bit, add printer tests

--- src/printer/index.js
+++ src/printer/index.js
@@ -1,11 +1,6 @@
 let stripAnsi = require('strip-ansi')
 let stripEntries = (([ k, v ]) => [ stripAnsi(k), stripAnsi(v) ])
-let c = require('chalk')
-
-// Look for color-disabling env vars
-let { BEGIN_NO_COLOR, NO_COLOR, TERM } = process.env
-let isTTY = !!(process.stdout.isTTY)
-let noColor = BEGIN_NO_COLOR || NO_COLOR || !isTTY || TERM === 'dumb'
+let getStack = err => err.stack.split('\n').slice(1).join('\n')
 
 // Per clig.dev: stdout is for piping to other apps; stderr is for userland
 //   https://clig.dev/#the-basics
@@ -17,11 +12,17 @@ function printer (params, out = '', verboseOrDebug) {
   // TODO: this should probably be more nuanced based on stdout vs stderr, TTY state, etc.
   if (args.quiet) return
 
+  // Look for color-disabling env vars
+  let { BEGIN_NO_COLOR, NO_COLOR, TERM } = process.env
+  let isTTY = !!(process.stdout.isTTY)
+  let noColor = BEGIN_NO_COLOR || NO_COLOR || !isTTY || TERM === 'dumb'
+
+  // JSON output is assumed to be for machines, not for userland (read: stderr)
   if (args.json && !verboseOrDebug) {
     let output
     if (isError) {
       output = { error: stripAnsi(out.message) }
-      if (args.debug) output.stack = stripAnsi(out.stack)
+      if (args.debug) output.stack = stripAnsi(getStack(out))
     }
     else output = out.json
       ? Object.fromEntries(Object.entries(out.json).map(stripEntries))
@@ -29,32 +30,29 @@ function printer (params, out = '', verboseOrDebug) {
     let log = isError ? console.error : console.log
     log(JSON.stringify(output, null, 2))
   }
-  else {
+  else if (!args.json && out) {
     let format = output => (noColor || args['no-color']) ? stripAnsi(output) : output
     if (isError) {
+      let c = require('chalk')
       console.error(format(`${c.bold.red('Error:')} ${out.message}`))
       if (args.debug) {
-        let stack = out.stack.split('\n').slice(1).join('\n')
+        let stack = getStack(out)
         console.error(format(stack))
       }
     }
-    else if (verboseOrDebug && out) {
-      console.error(format(out))
-    }
-    else if (out) {
-      console.log(format(out.stdout || out))
+    else {
+      let log = verboseOrDebug ? console.error : console.log
+      log(format(out.string || out))
     }
   }
 }
 
 printer.verbose = function (params, out) {
-  let { args } = params
-  let { verbose, debug } = args
+  let { verbose, debug } = params.args
   if (verbose || debug) printer(params, out, true)
 }
 printer.debug = function (params, out) {
-  let { args } = params
-  let { debug } = args
+  let { debug } = params.args
   if (debug) printer(params, out, true)
 }
 
