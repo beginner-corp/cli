7f30fb43753982a8550111ce764863e43e352c84
macdonst <simon.macdonald@gmail.com>
2022-07-04 15:02:25 -0400

    Fix incorrect help information
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,7 +3,7 @@ let backtickify = a => a.map(s => '`' + s + '`').join(', ')
 function checkManifest (inventory) {
   let c = require('picocolors')
   if (!inventory.inv._project.manifest) {
-    let message = `No Begin project found! To create one, run: ${c.white(c.bold('begin new app'))}`
+    let message = `No Begin project found! To create one, run: ${c.white(c.bold('begin new project'))}`
     return Error(message)
   }
   let { parse } = require('path')
