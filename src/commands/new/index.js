d9bec71512b62fba09a74e6204e0c952ee0c59b1
macdonst <simon.macdonald@gmail.com>
2022-07-11 16:18:18 -0400

    Remove "begin new <project-name>"
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- src/commands/new/index.js
+++ src/commands/new/index.js
@@ -1,8 +1,6 @@
 let names = { en: [ 'new', 'gen' ] }
 let subcommands = [ 'project', 'http', 'event', 'scheduled' ]
 let help = require('./help').bind({}, subcommands)
-let { existsSync, mkdirSync } = require('fs')
-let { join } = require('path')
 
 async function runAction (actionName, params) {
   let generator = require(`./generators/${actionName}`)
@@ -22,29 +20,6 @@ async function action (params) {
   let subcommand = params.args._[1]
   if (subcommands.includes(subcommand)) {
     return runAction(subcommand, params)
-  }
-  else if (subcommand) {
-    // Error out if folder already exists
-    if (existsSync(subcommand)) {
-      let err = Error('Please specify a new folder to create a project in')
-      if (subcommand) err = Error(`Invalid folder: ${subcommand} already exists`)
-      err.type = '__help__'
-      throw err
-    }
-
-    // Create new project
-    let cwd = process.cwd()
-    let projectDir = join(cwd, subcommand)
-    if (mkdirSync(projectDir, { recursive: true })) {
-      process.chdir(subcommand)
-      return runAction('project', params)
-    }
-    else {
-      let err = Error('Unable to create project folder')
-      if (subcommand) err = Error(`Unable to create project folder: ${subcommand}`)
-      err.type = '__help__'
-      throw err
-    }
   }
   else {
     let err = Error('Please specify a resource type to create')
