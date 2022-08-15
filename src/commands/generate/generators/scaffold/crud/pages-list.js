74abbe192f57fa3e4b8128e180a0eefb33b13f19
macdonst <simon.macdonald@gmail.com>
2022-08-15 16:12:44 -0400

    Create list html at generate time
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- src/commands/generate/generators/scaffold/crud/pages-list.js
+++ src/commands/generate/generators/scaffold/crud/pages-list.js
@@ -1,23 +1,39 @@
-module.exports = function ({ plural, singular }) {
-  return `// View documentation at: https://docs.begin.com
-const rows = (item = {}) => {
-  return \`\${Object.keys(item).map((key) => {
-      if (typeof item[key] === 'object') {
-          return rows(item[key])
-      } else {
-          return \`<p><strong>\${key}: </strong>\${item[key]}</p>\`
-      }
-  }).join('')}\`
+function schemaToList ({ schema = {}, prefix = '' }) {
+  return Object.keys(schema.properties).map(key => {
+    console.log(schema.properties[key])
+    if (schema.properties[key].type === 'object') {
+      return schemaToList({ schema: schema.properties[key], prefix: `.${key}?` })
+    }
+    else {
+      return `<p><strong>${key}: </strong>\${item?${prefix}.${key}}</p>`
+    }
+  }
+  ).join('\n  ')
 }
 
+module.exports = function ({ plural, singular, schema }) {
+  console.log('create list', schema)
+  let list = schemaToList({ schema })
+
+  return `// View documentation at: https://docs.begin.com
 export default function Html ({ html, state }) {
   const { store } = state
-  let ${plural} = store.${plural}?.map(item=> \`<div>\${rows(item)}</div><p><a href="/${plural}/\${item.key}">Edit this ${singular}</a></p><p><form action="/${plural}/\${item.key}/delete" method="POST"><button>Delete this ${singular}</button></form></p>\`).join('')
+  let ${plural} = store.${plural} || []
 
   return html\`
   <section>
     <h1>Index page</h1>
-    \${${plural}}
+    \${${plural}.map(item => \`<div>
+  ${list}
+</div>
+<p>
+  <a href="/${plural}/\${item.key}">Edit this ${singular}</a>
+</p>
+<p>
+  <form action="/${plural}/\${item.key}/delete" method="POST">
+    <button>Delete this ${singular}</button>
+  </form>
+</p>\`).join('\\n')}
     <div>
       <a href="/${plural}/new">New ${singular}</a>
     </div>
