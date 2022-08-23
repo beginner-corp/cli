15b3c8cd6afeb3358db2549e8f8085e1aee1bc94
Ryan Bethel <ryan.bethel@gmail.com>
2022-08-23 09:31:49 -0400

    error handling for multiple gen auth calls, refactor for other types of auth

--- src/commands/generate/generators/auth/action.js
+++ src/commands/generate/generators/auth/action.js
@@ -1,9 +1,25 @@
 module.exports = async function action (params, utils, command) {
+  let { args } = params
+  let error = require('./errors')(params, utils)
   let project = params.inventory.inv._project
   let generate = require('../_generate')
 
-  let manifest = require('./manifest')
 
-  // Run the generic generator
-  generate({ manifest, command, project, utils })
+  let plugins = project.arc.plugins
+  if (plugins.includes('arc-plugin-oauth')) {
+    return error('oauth_plugin_already_installed')
+  }
+
+  let authType = args.t || args.type
+  if (!authType || authType === 'oauth') {
+    let manifest = require('./oauth-manifest')
+    generate({ manifest, command, project, utils })
+  }
+  else if (authType === 'magic-link') {
+    // TODO: add prefs for seed DB
+
+    let manifest = require('./magic-manifest')
+    generate({ manifest, command, project, utils })
+  }
+
 }
