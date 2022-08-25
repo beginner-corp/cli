8b1fcaa1acc33ffa2d7d486652df4e15bbe3e062
macdonst <simon.macdonald@gmail.com>
2022-08-25 00:25:34 -0400

    Remove workaround for issue #29
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- src/commands/dev/index.js
+++ src/commands/dev/index.js
@@ -5,19 +5,11 @@ let c = require('picocolors')
 async function action (params) {
   let { appVersion, args } = params
   let { checkManifest } = require('../../lib')
-  let { mkdirSync, existsSync } = require('fs')
 
   let _inventory = require('@architect/inventory')
   let inventory = await _inventory()
   let manifestErr = checkManifest(inventory)
   if (manifestErr) return manifestErr
-
-  // Work around for https://github.com/beginner-corp/cli/issues/29
-  const sharedFolder = inventory?.inv?.shared?.src
-  const arcPluginEnhance = Object.keys(inventory?.inv?.plugins).includes('enhance/arc-plugin-enhance')
-  if (!existsSync(sharedFolder) && arcPluginEnhance) {
-    mkdirSync('models')
-  }
 
   let cli = require('@architect/sandbox/src/cli')
   let { debug, quiet, verbose } = args
