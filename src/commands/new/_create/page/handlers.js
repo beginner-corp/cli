5107f1a33797f84fc38de00dd0983e5802b7c551
macdonst <simon.macdonald@gmail.com>
2022-08-30 20:01:43 -0700

    Add css to new page
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- src/commands/new/_create/page/handlers.js
+++ src/commands/new/_create/page/handlers.js
@@ -8,16 +8,16 @@ let javascript = lang => `// ${copy[lang].view_docs}
 export default function Html ({ html, state }) {
   // eslint-disable-next-line
   const { attrs, store } = state
-  return html\`<section>
-    <h1>Index page</h1>
+  return html\`<section class="m-auto p2 color-light font-sans">
+    <h1 class="mb1 font-semibold text3">Index page</h1>
     <p>html content for home page here</p>
   </section>
 \`
 }
 `
 
-let html = () => `<section>
-  <h1>Index page</h1>
+let html = () => `<section class="m-auto p2 color-light font-sans">
+  <h1 class="mb1 font-semibold text3">Index page</h1>
   <p>html content for home page here</p>
 </section>
 `
