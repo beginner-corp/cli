7869e9f6d48cffcb63487441547091be9c9fbd38
Ryan Block <rblock@gmail.com>
2022-10-14 09:44:13 -0700

    Hopefully fix client IDs in production

--- src/_main.js
+++ src/_main.js
@@ -6,8 +6,5 @@ let { join } = require('path')
 let commitFile = join(__dirname, '..', 'commit')
 let version = readFileSync(commitFile).toString()
 
-let clientIDFile = join(__dirname, '..', 'client-ids.json')
-let clientIDs = JSON.parse(readFileSync(clientIDFile))
-
 let begin = require('./')
-begin({ version, clientIDs })
+begin({ version })
