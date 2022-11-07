e85c78eac6bf302cd3bb421cc2effcfb019589d7
Ryan Bethel <ryan.bethel@gmail.com>
2022-11-07 00:01:22 -0500

    user to account

--- src/commands/generate/generators/auth/handlers/magic/models/auth/auth-check.js
+++ src/commands/generate/generators/auth/handlers/magic/models/auth/auth-check.js
@@ -1,16 +1,49 @@
 module.exports = function () {
-  return `export default function (req, role) {
+  return `export function checkRole(req, role) {
   const { session = {} } = req
   const account = session.account
-  const userRoles = Object.values(account?.user?.roles || {} )
+  const accountRoles = Object.values(account?.account?.roles || {} )
 
   if (!role) {
     return account
   }
-  else if (userRoles?.includes(role)) {
+  else if (accountRoles?.includes(role)) {
     return true
   }
   else return false
 }
+
+export function checkAccount(req) {
+  const { session = {} } = req
+  const account = session?.account
+  return account
+}
+
+export function checkPermissions(req, operation) {
+  const { session = {} } = req
+  const account = session.account
+  const accountPermissions = account?.permissions?.filter(Boolean)
+  let rules = accountPermissions?.map(i => i.rules).map((role) => {
+    let newRules = []
+    for (const property in role) {
+      const index = Number.parseInt(property.replace(/([^_]*)_(\\d*)/gm, '$2')) - 1
+      const newProperty = property.replace(/([^_]*)_(\\d*)/gm, '$1')
+      newRules[index] = newRules[index] || {}
+      newRules[index][newProperty] = role[property]
+    }
+    return newRules
+  }).flat()
+
+  const match = rules?.filter(rule => {
+    return ((operation.action === rule.action)
+      && (operation.target === rule.target)
+      && (operation.owner === rule.owner)
+      && ((operation?.id === rule?.id) || (!operation?.id && !rule?.id)))
+  })
+  return match?.length > 0
+
+}
+
+
 `
 }
