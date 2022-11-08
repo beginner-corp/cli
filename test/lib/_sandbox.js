cb01f0d40c1cce14a8ec961477d975883368c491
Ryan Block <rblock@gmail.com>
2022-11-08 14:59:55 -0800

    Additional port conflict fixes as we've added more simultaneous self-hosted runners But like, not on Windows, because it reserves ports in the unreserved port block, because Windows

--- test/lib/_sandbox.js
+++ test/lib/_sandbox.js
@@ -22,10 +22,11 @@ function getPort (checking) {
           checking++
           check()
         }
+        else rej(err)
       })
       tester.once('listening', () => {
         tester.close(() => {
-          // Tester close emits multiple events, so only resolve once
+          // server.close() may call back multiple times, so only resolve once
           if (!done) {
             done = true
             res(checking)
@@ -39,9 +40,19 @@ function getPort (checking) {
 
 module.exports = {
   async start ({ cwd }) {
-    let port = await getPort(3333)
+    // Our self-hosted runners do not (yet) have full isolation, so port conflicts may be a thing
+    // For whatever reason, even relying on port checking / incrementing wasn't reliably working in that environment
+    // So, instead we'll just get a random port number between 50000–65000 and use that
+    let min = 50000
+    let max = 65000
+    let seed = process.env.CI && !process.platform.startsWith('win')
+      ? Math.floor(Math.random() * (max - min + 1)) + min
+      : 3333
+    let port = await getPort(seed)
     // TODO: fix Sandbox (and this) to accept a ports property at some point?
-    process.env.ARC_INTERNAL_PORT = port + 100 // Yeah, this magic number can definitely break, but it's unlikely in a CI context
+    let _arc = port + 100 // Yeah, this magic number can definitely break, but it's pretty unlikely in a CI context
+    process.env.ARC_INTERNAL_PORT = _arc
+    console.log('Starting Sandbox with ports:', { http: port, _arc })
     await sandbox.start({ cwd, port, quiet: true })
     return port
   },
