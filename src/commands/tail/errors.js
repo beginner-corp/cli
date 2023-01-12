a0e7b9699c78657a174bf2955bc28fba7f8bb35b
Ryan Block <rblock@gmail.com>
2023-01-12 13:54:55 -0800

    Adds `begin tail` command

---
+++ src/commands/tail/errors.js
@@ -0,0 +1,13 @@
+module.exports = function error (params) {
+  return function (err) {
+    let { lang } = params
+    let errors = {
+      en: {
+        app_not_found: 'No app found with that app ID',
+        no_env: 'Environment ID not found, please run with -e or --envID',
+        invalid_env: 'Invalid or incorrect environment ID',
+      }
+    }
+    return Error(errors[lang][err])
+  }
+}
---
+++ src/commands/tail/help.js
@@ -0,0 +1,43 @@
+module.exports = {
+  en: {
+    usage: [ 'tail <parameters>', '[options]' ],
+    description: 'Tail your Begin app logs in real-time',
+    contents: {
+      header: 'Tail logs',
+      items: [
+        {
+          name: '-a, --app <ID>',
+          description: `App containing the environment`,
+          optional: true,
+        },
+        {
+          name: '-e, --env <name|ID>',
+          description: `App environment to list logs from`,
+          optional: true,
+        },
+        {
+          name: '-f, --filter',
+          description: `Filter to apply against logs`,
+        },
+      ],
+    },
+    examples: [
+      {
+        name: 'Tail logs (from within a single environment app)',
+        example: 'begin tail',
+      },
+      {
+        name: 'Tail logs (specifying an environment name)',
+        example: 'begin logs --env staging',
+      },
+      {
+        name: 'Tail logs with filter',
+        example: 'begin logs --filter "GOT HERE"',
+      },
+      {
+        name: 'Tail logs with grep',
+        example: 'begin logs | grep "GOT HERE"',
+      },
+    ]
+  }
+}
