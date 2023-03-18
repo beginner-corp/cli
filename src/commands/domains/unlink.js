b55b300c86abd0446af822973413079e892b2816
Taylor Beseda <tbeseda@gmail.com>
2023-03-18 01:18:52 -0600

    domain unlink doesn't require app and env IDs

--- src/commands/domains/unlink.js
+++ src/commands/domains/unlink.js
@@ -6,24 +6,18 @@ async function action (params) {
 
   if (!domain || (typeof domain === 'string' && domain.length === 0))
     return Error('Please specify a domain with --domain')
-  if (!appID || (typeof appID === 'string' && appID.length === 0))
-    return Error('Please specify an app ID with --app')
-  if (!env || (typeof env === 'string' && env.length === 0))
-    return Error('Please specify an environment with --env')
-
-  let theApp = await client.find({ token, _staging, appID })
-  if (!theApp)
-    return Error(`Unable to find App with id <${appID}>`)
-
-  let theEnv = theApp.environments.find(({ name, envID }) => [ name, envID ].includes(env))
-  if (!theEnv)
-    return Error(`App <${appID}> does not have an environment "${env}"`)
 
   let domains = await client.domains.list({ token, _staging })
   let theDomain = domains.find(d => d.domain === domain)
 
-  if (!theDomain)
-    return Error(`Unable to find domain "${domain}"`)
+  if (!theDomain) return Error(`Unable to find domain "${domain}"`)
+
+  let envID
+  if (appID && env) {
+    let theApp = await client.find({ token, _staging, appID })
+    let theEnv = theApp.environments.find(({ name, envID }) => [ name, envID ].includes(env))
+    envID = theEnv.envID
+  }
 
   // TODO: confirm with user; it can take time to unlink a domain
 
@@ -31,14 +25,14 @@ async function action (params) {
     token,
     _staging,
     domainID: theDomain.domainID,
-    appID: theApp.appID,
-    envID: theEnv.envID,
+    appID,
+    envID,
   })
 
   if (result.success)
     return [
-      `${c.cyan(domain)} is being unlinked from the ${c.bold(theApp.name)} app's "${theEnv.name}" env`,
-      'It may take several minutes before ${c.cyan(domain)} is available to re-link.',
+      `${c.cyan(domain)} is being unlinked`,
+      `It may take several minutes before ${c.cyan(domain)} is available to re-link.`,
     ].join('\n')
   else
     return Error('Unable to unlink domain. Please try again later.')
