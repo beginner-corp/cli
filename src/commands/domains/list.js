37c750b50e96ad9b6ebebefd994657477a8d6dab
Taylor Beseda <tbeseda@gmail.com>
2023-07-27 11:06:14 -0600

    domains list table

--- src/commands/domains/list.js
+++ src/commands/domains/list.js
@@ -12,47 +12,52 @@ async function action (params) {
   let apps = await client.list({ token, _staging })
 
   let presentDate = (date) => (new Date(date)).toLocaleString()
-  let mark = c.gray(' └──')
-  let arrow = c.cyan(' └──')
-  let output = []
-  domains.forEach(({ domain, domainID, status, appLink, r53LastStatus, updatedAt }) => {
-    if (status === states.PURCHASING) return
+  let Table = require('cli-table3')
+  let table = new Table({ head: [ 'Domain', 'Status', 'Updated' ] })
+  for (const { domain, domainID, status, appLink, r53LastStatus, updatedAt } of domains) {
+    if (status === states.PURCHASING) continue
+    let row = [ ]
 
-    let domainLine = c.underline(c.cyan(`https://${domain}`))
-    if (verbose) domainLine += ` <${domainID}> ${c.bold(status)} [updated: ${presentDate(updatedAt)}]`
-    output.push(domainLine)
+    if (verbose) row.push(`${c.bold(domain)} <${domainID}>`)
+    else row.push(c.bold(domain))
 
     if (status === states.LINKED && appLink) {
       let { appID, envID } = appLink
       let theApp = apps.find(a => a.appID === appID)
       let theEnv = theApp.environments.find(e => e.envID === envID)
 
-      let linkLine = `${arrow} ${c.bold(c.green(theApp.name))}`
-      if (verbose) linkLine += ` <${appID}>`
-      linkLine += `: "${theEnv.name}"`
-      if (verbose) linkLine += ` <${envID}>`
-      output.push(linkLine)
+      let linkedStatus = `${c.bold(c.green(theApp.name))}`
+      if (verbose) linkedStatus += ` <${appID}>`
+      linkedStatus += `: "${theEnv.name}"`
+      if (verbose) linkedStatus += ` <${envID}>`
+      row.push(linkedStatus)
     }
     else if (status === states.REGISTERING) {
-      output.push(`${mark} ${c.bold(`Registration: ${r53LastStatus}`)}`)
+      row.push(`${c.bold(`Registration: ${r53LastStatus}`)}`)
     }
     else if (status === states.ACTIVE) {
-      output.push(`${mark} ${c.bold('Available to link with "begin domains link"')}`)
+      row.push(`${c.bold('Available to link')}`)
     }
     else if (status === states.LINKING) {
-      output.push(`${mark} ${c.bold('Linking to an app environment')}`)
+      let linkingStatus = `${c.bold('Linking to an app environment')}`
       if (verbose)
-        output.push(`    ${mark} Last DNS check: ${c.bold(r53LastStatus)}`)
+        linkingStatus += ` Last DNS check: ${c.bold(r53LastStatus)}`
+      row.push(linkingStatus)
     }
     else if (status === states.UNLINKING) {
-      output.push(`${mark} ${c.bold('Unlinking DNS and SSL certificates')}`)
+      row.push(`${c.bold('Unlinking DNS and SSL certificates')}`)
     }
     else if ([ states.LAPSED, states.CANCELLED, states.DELETED ].includes(status)) {
-      output.push(`${mark} ${c.bold('Inactive')} (status: ${status})`)
+      row.push(`${c.bold('Inactive')} (status: ${status})`)
     }
-  })
+    else {
+      row.push(`${c.bold('Unknown')} (status: ${status})`)
+    }
 
-  return output.join('\n')
+    table.push(row.concat([ presentDate(updatedAt) ]))
+  }
+
+  return table.toString()
 }
 
 module.exports = {
