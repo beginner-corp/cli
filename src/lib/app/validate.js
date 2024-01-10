eb700b3f1c7d9092bac0e92100a189f5fe74c2b8
Taylor Beseda <tbeseda@gmail.com>
2024-01-10 11:07:37 -0700

    disallow testing app env name with improved error message

--- src/lib/app/validate.js
+++ src/lib/app/validate.js
@@ -1,14 +1,20 @@
 let looseName = /^[a-z][a-zA-Z0-9-_]+$/
+let cannotBeTesting = [
+  'Environment name cannot be "testing".',
+  'This may conflict with a local testing environment.',
+].join('\n')
 let envRequired = 'An environment name is required'
 let envInvalid = 'Environment name must be [a-zA-Z0-9-_]'
 
 let validateEnvName = {
   arg: name => {
+    if (name === 'testing') throw ReferenceError(cannotBeTesting)
     if (!name) throw ReferenceError(envRequired)
     if (!looseName.test(name)) throw ReferenceError(envInvalid)
     return
   },
   prompt: name => {
+    if (name === 'testing') return cannotBeTesting
     if (!name) return envRequired
     return looseName.test(name)
   }
