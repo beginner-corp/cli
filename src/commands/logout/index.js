2b66092b50dd2cbc9bdf6e79e8f52444ce73a4be
Taylor Beseda <tbeseda@gmail.com>
2024-01-23 12:51:48 -0700

    logout always updates local config
    fixes #166

--- src/commands/logout/index.js
+++ src/commands/logout/index.js
@@ -21,7 +21,7 @@ async function action (params) {
     return Error('Config file not found, cannot log out of Begin session')
   }
 
-  let config = JSON.parse(readFileSync(configPath))
+  let config = JSON.parse(readFileSync(configPath).toString())
   let { access_token, stagingAPI } = config
   let { __BEGIN_TEST_URL__ } = process.env
   let domain = __BEGIN_TEST_URL__
@@ -41,6 +41,7 @@ async function action (params) {
 
   let tiny = require('tiny-json-http')
   let url = domain + `/clients/${clientID}/token/delete`
+  let message = ''
   try {
     let authorization = `Bearer ${access_token}`
     await tiny.post({
@@ -48,16 +49,23 @@ async function action (params) {
       headers: { authorization },
       body: { access_token },
     })
-
+  }
+  catch (err) {
+    if (err?.body?.errors?.includes('token_invalid_or_expired'))
+      message = 'Could not revoke access token, it may have already expired.'
+    else
+      // err can be vague here; try to get a more specific message
+      message = err?.body?.error || err?.body?.errors || err?.message || err || 'Unknown error'
+    message += '\n'
+  }
+  finally {
     let now = new Date().toISOString()
     config.modified = now
     delete config.access_token
     delete config.device_code
     writeFile(configPath, JSON.stringify(config, null, 2))
-    return 'Successfully logged out!'
+    message += 'Successfully logged out!'
   }
-  catch (err) {
-    if (err?.body?.errors?.includes('token_invalid_or_expired')) return Error('Invalid client ID or access token')
-    return err
-  }
+
+  return message
 }
