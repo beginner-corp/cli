39ed858a9088287b1b3c8922c3713dc92c0f62f3
Taylor Beseda <tbeseda@gmail.com>
2024-03-14 15:26:03 -0600

    Revert "Debug message for deploy"
    This reverts commit b6c37627cb3b2df7aefd71274a4c0c807a9207bf.

--- src/commands/deploy/index.js
+++ src/commands/deploy/index.js
@@ -12,30 +12,23 @@ async function action (params) {
   let config = getConfig(params)
   if (!config.access_token) {
     let msg = 'You must be logged in to deploy to Begin, please run: begin login'
-    console.log(msg)
     return Error(msg)
   }
-  console.log('got access token')
 
   let manifestErr = checkManifest(params.inventory)
   if (manifestErr) return manifestErr
-  console.log('manifest fine')
 
   let appID
   try {
     appID = getAppID(params.inventory, args)
-    console.log('appID', appID)
   }
   catch (error) {
-    console.log('no appID', error)
     appID = null
   }
 
   // Pass along any specified environment IDs
   let env = args.env || args.e
   let envName = env !== true && env || undefined
-
-  console.log('call deploy')
 
   let utils = {
     ...lib,
