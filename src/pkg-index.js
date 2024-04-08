6bf35fd97dd70cb96c088341d3687cfd0c43aaf6
Taylor Beseda <tbeseda@gmail.com>
2024-04-08 10:25:13 -0600

    only print deprecation if tty

--- src/pkg-index.js
+++ src/pkg-index.js
@@ -1,7 +1,8 @@
 #! /usr/bin/env node
 
-process.stderr.write('\x1b[41m\x1b[37m\x1b[1m DEPRECATION NOTICE: \x1b[0m \x1b[31m\x1b[1mThe Begin CLI is now available via npm\x1b[0m\n')
-process.stderr.write('\x1b[1mPlease run "npm install -g @begin/deploy" to install the latest version\x1b[0m\n')
+if (process.stdin.isTTY) {
+  process.stderr.write('\x1b[41m\x1b[37m\x1b[1m DEPRECATION NOTICE: \x1b[0m \x1b[31m\x1b[1mThe Begin CLI is now available via npm\x1b[0m\n')
+  process.stderr.write('\x1b[1mPlease run "npm install -g @begin/deploy" to install the latest version\x1b[0m\n')
+}
 
-const begin = require('./index')
-begin()
+require('./index')()
