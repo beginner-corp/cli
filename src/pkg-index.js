997f184e4d93e2da3fa9ef7d555ceb41818e90d5
Taylor Beseda <tbeseda@gmail.com>
2024-04-16 17:45:54 -0600

    deprecation notice from printer in main begin fn

--- src/pkg-index.js
+++ src/pkg-index.js
@@ -1,13 +1,5 @@
 #! /usr/bin/env node
 
-if (process.env.NODE_ENV !== 'testing') {
-  console.log('console.log')
-  console.error('console.error')
-  process.stderr.write('process.stderr.write\n')
-  process.stdout.write('process.stdout.write\n')
-
-  console.error('\x1b[41m\x1b[37m\x1b[1m DEPRECATION NOTICE: \x1b[0m \x1b[31m\x1b[1mThe Begin Deploy CLI is now updated via npm\x1b[0m')
-  console.error('\x1b[1mPlease run "npm install -g @begin/deploy" to install the latest version\x1b[0m\n')
-}
+process.env.BEGIN_CLI_TYPE = 'BINARY'
 
 require('./index')()
