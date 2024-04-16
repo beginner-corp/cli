32ee16317beb1fef43277310e90231bebee31518
Taylor Beseda <tbeseda@gmail.com>
2024-04-16 11:00:08 -0600

    deprecate update command

--- src/commands/update/help.js
+++ src/commands/update/help.js
@@ -1,16 +1,6 @@
 module.exports = {
   en: {
-    usage: [ 'update', '[options]' ],
-    description: 'Update Begin to the latest version',
-    contents: {
-      header: 'Update options',
-      items: [
-        {
-          name: '--use',
-          description: 'Switch between `latest` (stable) and `main` (canary) channels',
-          optional: true,
-        },
-      ],
-    },
+    usage: [ 'update' ],
+    description: '(deprecated) Update Begin to the latest version',
   },
 }
