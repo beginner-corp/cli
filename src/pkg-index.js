d1b440f78ede0de04a39cc919b2aeaad72818a17
Taylor Beseda <tbeseda@gmail.com>
2024-04-16 11:35:48 -0600

    Update pkg-index.js

--- src/pkg-index.js
+++ src/pkg-index.js
@@ -1,8 +1,10 @@
 #! /usr/bin/env node
 
+console.log(process.stdin.isTTY ? '•' : '')
+
 if (process.stdin.isTTY) {
-  process.stderr.write('\n\x1b[41m\x1b[37m\x1b[1m DEPRECATION NOTICE: \x1b[0m \x1b[31m\x1b[1mThe Begin Deploy CLI is now updated via npm\x1b[0m\n')
-  process.stderr.write('\x1b[1mPlease run "npm install -g @begin/deploy" to install the latest version\x1b[0m\n\n')
+  console.log('\x1b[41m\x1b[37m\x1b[1m DEPRECATION NOTICE: \x1b[0m \x1b[31m\x1b[1mThe Begin Deploy CLI is now updated via npm\x1b[0m')
+  console.log('\x1b[1mPlease run "npm install -g @begin/deploy" to install the latest version\x1b[0m\n')
 }
 
 require('./index')()
