dcd514078670de710a19b1b19cdf6409bb0dc7e2
Taylor Beseda <tbeseda@gmail.com>
2024-05-21 08:45:45 -0600

    deploy always returns unhandled error

--- src/commands/deploy/action.js
+++ src/commands/deploy/action.js
@@ -22,7 +22,7 @@ module.exports = async function action (params, utils) {
     catch (err) {
       if (err.message === 'app_not_found') return Error(`No app found with app ID '${appID}'`)
       if (err.message === 'unknown_error') return Error(`No app found with app ID '${appID}' or the user does not have permission to access the app.`)
-      return
+      return err
     }
   }
 
