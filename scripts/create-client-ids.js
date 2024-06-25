e1648cba38bb3319ca303d5fa1ed60fdf3876562
Taylor Beseda <tbeseda@gmail.com>
2024-06-25 08:40:25 -0600

    don't publish all the things to npm

--- scripts/create-client-ids.js
+++ scripts/create-client-ids.js
@@ -1,10 +1,13 @@
 #! /usr/bin/env node
 
+const process = require('node:process')
 const { join } = require('node:path')
 const { writeFileSync } = require('node:fs')
 
-const staging = process.env.BEGIN_CLI_CLIENT_ID_STAGING
-const production = process.env.BEGIN_CLI_CLIENT_ID_PRODUCTION
+const {
+  BEGIN_CLI_CLIENT_ID_STAGING: staging,
+  BEGIN_CLI_CLIENT_ID_PRODUCTION: production,
+} = process.env
 
 if (!staging || !production) {
   throw ReferenceError('Missing staging and/or production client ID')
